f29477af5b413795e448a0cf036c1a4835320fda
Bruno de Souza Farias <108137122+BrunoSouzaFarias@users.noreply.github.com>
2024-09-06 23:12:34 -0300

    Feat:SpeedInsights

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
 import ExcelUpload from "../../components/ExcelUpload/ExcelUpload";
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import "./Home.css";
 
 function Home() {
   return (
     <div className="home-container">
       <h1>Cadastro de Usuários</h1>
+      <SpeedInsights />
       <RegistrationForm />
       <ExcelUpload />
     </div>
